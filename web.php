bf19de6ff93cb8b4aba73a3014b3159a6053651f
Xsayfex <100242931+Abdulaziz-Arawi@users.noreply.github.com>
2022-03-12 18:55:45 +0300

    Add files via upload

---
+++ web.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>LoginPage</title>
+
+</head>
+<body>
+    
+
+    <div class="header">
+        <div class="container">
+
+            <h1> Login </h1>
+                
+        </div>
+
+    </div>
+
+        <div class="form1">
+            <div class="container">
+
+                <form action="check.php" method="post">
+                    <input class="input" type="text" placeholder="Your Name" name="name">
+                    <input class="input" type="password" placeholder="Your password" name="password">
+                    <input class="butnnn" type="Submit" name="btn" value="Send">
+                </form>
+
+
+            </div>
+
+        </div>
+
+    <div class="father">
+        <div class="home">
+
+            <h1> My Name Is 'Abdulaziz' </h1>
+
+        </div>
+    </div>
+
+    
+
+
+</body>
+</html>
